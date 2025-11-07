5bf291f563175bfd155b0b8800235d76d53d208a
Veeti33 <veeti.hamalainen@gmail.com>
2025-11-07 12:16:29 +0200

    login sivu, HTML/CSS

---
+++ frontend/pages/signin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="frontend\styles\pages\signin.css">
+    <title>Kirjaudu sisään</title>
+</head>
+<body>
+    <div class="login-cont">
+        <div class="card">
+            <div class="card-left">
+                <h1>Luokkavaraus</h1>
+                <p>Filler filler filler filler filler</p>
+            </div>
+            <div class="card-right">
+                <div class form="login-form">
+                    <form action="#" method="#">
+                        <label for="user">Käyttäjätunnus</label>
+                        <input type="text" name="user" required>
+                        <label for="pwd">Salasana</label>
+                        <input type="password" name="pwd" required>
+                        <button type="submit">Kirjaudu sisään</button>
+                    </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
