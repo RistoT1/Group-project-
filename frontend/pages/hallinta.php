a19cf10d60cb5f286d95e0f513159a1dfebc1a85
Julius <julius.Vahanikkial@edu.sakky.fi>
2025-11-10 12:32:11 +0200

    hallinta

---
+++ frontend/pages/hallinta.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Käyttäjien hallinta - Luokkavarausjärjestelmä</title>
+    <link rel="stylesheet" href="signin.css">
+    <style>
+    </style>
+</head>
+<body>
+    <nav>
+        <button>+ Lisää uusi käyttäjä</button>
+        <button>Käyttäjät</button>
+        <button>Luokkahuoneet</button>
+        <button>Varauskatsaus</button>
+        <button>Kirjaudu ulos</button>
+        <button>Edellinen</button>
+              <ul>
+                <li><a href="#">Kaikki käyttäjät</a></li>
+                <li><a href="#">Opettajat</a></li>
+                <li><a href="#">Ylläpitäjät</a></li>
+              </ul>
+               <label>NIMI</label>
+               <label>ROOLI</label>
+               <label>VIIMEKSI AKTIIVINEN</label>
+               
+    <section class="text-center">
+        <h1>Luokkavaraus</h1>
+        <p></p>
+    </section>
+
+    <section class="text-center">
+        <h1><center>Käyttäjien hallinta</center></h1>
+    </section>
+</main>
+</body>
+</html>
