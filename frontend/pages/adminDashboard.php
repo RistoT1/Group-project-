aa3179ee50d820ccdd785b38911b0b586c1cf8bc
Julius <julius.Vahanikkial@edu.sakky.fi>
2025-11-11 10:04:24 +0200

    hallinta paneeli

--- frontend/pages/adminDashboard.php
+++ frontend/pages/adminDashboard.php
@@ -1,16 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="../styles/pages/signin.css">
+    <link rel="stylesheet" href="../styles/root.css">
+    <title>Käyttäjien hallinta</title>
 </head>
-<body>
-    <section class="hero-section"> 
-        <div class="">
 
-        </div>
+<body>
+    <nav>
+        <button>+ Lisää uusi käyttäjä</button>
+        <button>Käyttäjät</button>
+        <button>Luokkahuoneet</button>
+        <button>Varauskatsaus</button>
+        <button>Kirjaudu ulos</button>
+        <button>Edellinen</button>
+              <ul>
+                <li><a href="#">Kaikki käyttäjät</a></li>
+                <li><a href="#">Opettajat</a></li>
+                <li><a href="#">Ylläpitäjät</a></li>
+              </ul>
+               <label>NIMI</label>
+               <label>ROOLI</label>
+               <label>VIIMEKSI AKTIIVINEN</label>
+
+               <input type="Find" name="Hae Käyttäjä..." placeholder="Hae Käyttäjä" required />
+               
+    <section class="text-center">
+        <h1>Luokkavaraus</h1>
+        <p></p>
     </section>
-    
+
+    <section class="text-center">
+        <h1><center>Käyttäjien hallinta</center></h1>
+    </section>
+</main>
 </body>
+<script type="module" src="../scripts/pages/ClassListPage.js"></script>
 </html>
